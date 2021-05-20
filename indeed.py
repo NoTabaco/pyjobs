c599105dabfa3ffe3d7d87a781006b72c6727b51
SevenStars <32127313+NoTabaco@users.noreply.github.com>
2021-05-20 23:29:15 +0900

    Updated Requesting Each Page

---
+++ indeed.py
@@ -0,0 +1,34 @@
+import requests
+from bs4 import BeautifulSoup
+
+LIMIT = 50
+BASE_URL = f"https://www.indeed.com/jobs?q=python&limit={LIMIT}"
+
+def extract_indeed_pages(url = BASE_URL, last_page = 0):
+    result = requests.get(url)
+    soup = BeautifulSoup(result.text, 'html.parser')
+
+    pagination_list = soup.find("ul", {"class": "pagination-list"})
+    links = pagination_list.find_all("a")
+    pages = []
+
+    if(len(links) == 5 or len(links) == 6):
+        for link in links:
+            if link.string is None: continue
+            pages.append(int(link.string))
+        last_page = pages[-1]
+        return extract_indeed_pages(f"{BASE_URL}&start={last_page * 50}", last_page)
+    elif last_page > 5 and len(links) < 5:
+        for link in links:
+            if link.string is None: continue
+            pages.append(int(link.string))
+        return last_page
+    else:
+        return len(links)
+
+def extract_indeed_jobs(last_page):
+    jobs = []
+    for page in range(last_page):
+        result = requests.get(f"{BASE_URL}&start={page*LIMIT}")
+        print(result.status_code)
+    return jobs
--- main.py
+++ main.py
@@ -1,30 +1,4 @@
-import requests
-from bs4 import BeautifulSoup
+from indeed import extract_indeed_pages, extract_indeed_jobs
 
-indeed_base_url = "https://www.indeed.com/jobs?q=python&limit=50"
-
-def get_pages(url, last_page):
-    indeed_result = requests.get(url)
-    indeed_soup = BeautifulSoup(indeed_result.text, 'html.parser')
-
-    pagination_list = indeed_soup.find("ul", {"class": "pagination-list"})
-    links = pagination_list.find_all("a")
-    pages = []
-
-    if(len(links) == 5 or len(links) == 6):
-        for link in links:
-            if link.string is None: continue
-            pages.append(int(link.string))
-        last_page = pages[-1]
-        return get_pages(f"https://www.indeed.com/jobs?q=python&limit=50&start={(last_page - 1) * 50}", last_page)
-    elif last_page > 5 and len(links) < 5:
-        for link in links:
-            if link.string is None: continue
-            pages.append(int(link.string))
-        return last_page
-    else:
-        return len(links)
-    
-total_page = get_pages(indeed_base_url, 1)
-
-print(total_page)
+last_indeed_page = extract_indeed_pages()
+indeed_jobs = extract_indeed_jobs(last_indeed_page)
