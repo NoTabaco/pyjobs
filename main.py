668dea3d3e6f70858bf1250e577f4e173be40591
SevenStars <32127313+NoTabaco@users.noreply.github.com>
2021-05-21 16:33:12 +0900

    Updated Extracting Companies

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 from indeed import extract_indeed_pages, extract_indeed_jobs
 
-last_indeed_page = extract_indeed_pages()
-indeed_jobs = extract_indeed_jobs(last_indeed_page)
+# last_indeed_page = extract_indeed_pages()
+indeed_jobs = extract_indeed_jobs(0)
